271645a3cee9336d42a774af30080cde030e7b1a
daisychen <daisyc2027@student.cis.edu.hk>
2024-08-27 14:34:55 +0800

    First Lab

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
 public class Dateable {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your age?");
+        String age = scanner.nextLine();
+
+        int minAge = Integer.parseInt(age);
+        System.out.println(age + "-year olds should date somebody who is at least " + (minAge / 2 + 7) + "years old");
         
     }
 }
+
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your age?");
+        int age = scanner.nextInt();
+
+        System.out.println(age + "year olds should be at least" + (100 - age) + "pages before giving up on a book");
     }
 }
